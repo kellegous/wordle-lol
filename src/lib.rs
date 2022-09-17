fd0adc6c072ebf58742ac141601550651161c1e4
Kelly Norton <kellegous@gmail.com>
2022-09-17 12:47:59 -0400

    .

--- src/lib.rs
+++ src/lib.rs
@@ -51,6 +51,10 @@ impl Word {
         &self.chars
     }
 
+    pub fn contains(&self, c: Char) -> bool {
+        self.chars.contains(&c)
+    }
+
     pub fn to_string(&self) -> String {
         self.chars().into_iter().map(|c| c.char()).collect()
     }
@@ -79,6 +83,15 @@ impl<'de> Deserialize<'de> for Word {
         d.deserialize_str(WordVisitor {})
     }
 }
+
+impl std::ops::Index<usize> for Word {
+    type Output = Char;
+
+    fn index(&self, ix: usize) -> &Self::Output {
+        &self.chars[ix]
+    }
+}
+
 struct WordVisitor;
 
 impl<'de> Visitor<'de> for WordVisitor {
@@ -96,5 +109,124 @@ impl<'de> Visitor<'de> for WordVisitor {
             Ok(word) => Ok(word),
             Err(e) => Err(E::custom(e)),
         }
+    }
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug, Hash)]
+#[repr(u8)]
+pub enum Directive {
+    Green,
+    Yellow,
+    Black,
+}
+
+impl Directive {
+    fn matches(&self, i: usize, guess: &Word, candidate: &Word) -> bool {
+        match self {
+            Directive::Green => guess[i] == candidate[i],
+            Directive::Yellow => guess[i] != candidate[i] && candidate.contains(guess[i]),
+            Directive::Black => !candidate.contains(guess[i]),
+        }
+    }
+
+    fn to_ascii_char(&self) -> char {
+        match self {
+            Directive::Green => 'g',
+            Directive::Yellow => 'y',
+            Directive::Black => 'b',
+        }
+    }
+
+    fn to_char(&self) -> char {
+        match self {
+            Directive::Green => '🟩',
+            Directive::Yellow => '🟨',
+            Directive::Black => '⬛',
+        }
+    }
+}
+
+#[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)]
+pub struct Feedback {
+    directives: [Directive; WORD_SIZE],
+}
+
+impl Feedback {
+    pub fn from_str(s: &str) -> Result<Feedback, Box<dyn Error>> {
+        if s.len() != WORD_SIZE {
+            return Err(format!("feedback must be {} characters", WORD_SIZE).into());
+        }
+        let mut directives = [Directive::Green; WORD_SIZE];
+        for (i, c) in s.char_indices() {
+            directives[i] = match c {
+                'g' | 'G' => Directive::Green,
+                'y' | 'Y' => Directive::Yellow,
+                'b' | 'B' => Directive::Black,
+                _ => return Err(format!("invalid directive: {}", c).into()),
+            };
+        }
+        Ok(Feedback { directives })
+    }
+
+    pub fn from_word(guess: &Word, solution: &Word) -> Feedback {
+        let mut directives = [Directive::Black; WORD_SIZE];
+        let mut claimed = [false; WORD_SIZE];
+
+        // mark greens
+        for (i, c) in guess.chars().iter().enumerate() {
+            if solution[i] == *c {
+                directives[i] = Directive::Green;
+                claimed[i] = true;
+            }
+        }
+
+        // mark yellows
+        for (i, c) in guess.chars().iter().enumerate() {
+            if directives[i] == Directive::Green {
+                continue;
+            }
+            for (j, k) in solution.chars().iter().enumerate() {
+                if !claimed[j] && *k == *c {
+                    directives[i] = Directive::Yellow;
+                    claimed[j] = true;
+                    break;
+                }
+            }
+        }
+
+        Feedback { directives }
+    }
+
+    pub fn to_string(&self) -> String {
+        self.directives().iter().map(|d| d.to_char()).collect()
+    }
+
+    pub fn to_ascii_string(&self) -> String {
+        self.directives()
+            .iter()
+            .map(|d| d.to_ascii_char())
+            .collect()
+    }
+
+    fn directives(&self) -> &[Directive] {
+        &self.directives
+    }
+
+    pub fn is_all_green(&self) -> bool {
+        self.directives.iter().all(|d| *d == Directive::Green)
+    }
+}
+
+impl std::ops::Index<usize> for Feedback {
+    type Output = Directive;
+
+    fn index(&self, ix: usize) -> &Self::Output {
+        &self.directives[ix]
+    }
+}
+
+impl std::fmt::Display for Feedback {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.to_string())
     }
 }
--- src/today/main.rs
+++ src/today/main.rs
@@ -1,11 +1,16 @@
 use std::error::Error;
 
 use chrono::Local;
-use wordle_lol::nytimes;
+use wordle_lol::{nytimes, Feedback};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let data = nytimes::Data::fetch()?;
     let solution = data.solution_on(Local::now().date().naive_local());
-    println!("{}", solution);
+
+    for word in data.guesses() {
+        let feedback = Feedback::from_word(word, &solution);
+        println!("{} {}", word, feedback);
+    }
+
     Ok(())
 }
